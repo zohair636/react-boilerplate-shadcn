47c959ff54e043d0566eafd1c3fec92b3db7ab19
zohair636 <hafiz.zohair2@gmail.com>
2026-04-22 10:40:18 +0500

    feat: reusable textarea component added
    reusable textarea component added with proper implementation of a11y

--- src/components/shared/error-boundary/common-error-boundary.tsx
+++ src/components/shared/error-boundary/common-error-boundary.tsx
@@ -16,7 +16,6 @@ class CommonErrorBoundary extends Component<Props, State> {
   };
 
   public static getDerivedStateFromError(error: Error): State {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true, error };
   }
 
