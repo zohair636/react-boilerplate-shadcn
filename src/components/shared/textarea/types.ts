8e45bc2da6c5dd54218bbcd4a2b8477dd60cc3a7
zohair636 <hafiz.zohair2@gmail.com>
2026-04-29 22:34:04 +0500

    refactor: input & textarea code structure quality improvement

--- src/components/shared/textarea/types.ts
+++ src/components/shared/textarea/types.ts
@@ -1,7 +1,6 @@
 import type { TextareaHTMLAttributes } from "react";
 
 export interface CommonTextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
-  // id?: string;
   label: string;
   description?: string;
   error?: string;
