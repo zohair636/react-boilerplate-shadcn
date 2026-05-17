30c78fdcfd5013bfc51c3a44abd13b5945735463
zohair636 <hafiz.zohair2@gmail.com>
2026-05-17 23:38:26 +0500

    feat: add reusable dialog component

--- src/pages/public/landing.tsx
+++ src/pages/public/landing.tsx
@@ -1,20 +1,7 @@
-import CommonButton from "@/components/shared/button";
-import { CommonDialog } from "@/components/shared/dialog";
-
 const Landing = () => {
   return (
     <div>
       <p>Landing Page</p>
-      <CommonDialog
-        trigger={<CommonButton label="Open Dialog" />}
-        title="Dialog"
-        description="This is common dialog"
-        showCloseControls={true}
-        confirm={{ label: "Proceed" }}
-        // cancel={{label: 'Cancel'}}
-      >
-        <h1>Common Dialog</h1>
-      </CommonDialog>
     </div>
   );
 };
