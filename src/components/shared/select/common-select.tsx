d3b35538c7218ef72df44d4f18f5dba58b335847
zohair636 <hafiz.zohair2@gmail.com>
2026-05-18 23:59:52 +0500

    feat: add reusable select component

---
+++ src/components/shared/select/common-select.tsx
@@ -0,0 +1,70 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import type { CommonSelectProps } from "./common-select.types";
+import { useId } from "react";
+import { Field, FieldError } from "@/components/ui/field";
+import CommonFieldLabel from "../label/field-label";
+
+const CommonSelect = ({
+  id,
+  options,
+  value,
+  onValueChange,
+  defaultValue,
+  label,
+  selectLabel,
+  className,
+  error,
+  required,
+}: CommonSelectProps) => {
+  const generatedId = useId();
+  const fieldId = id ?? generatedId;
+  const isInvalid = !!error && !value;
+
+  if (!options?.length) return null;
+
+  const placeholderOption = options.find((opt) => opt.value === null)?.label;
+  const renderOptions = options.filter((opt) => opt.value !== null) as {
+    label: string;
+    value: string;
+  }[];
+
+  return (
+    <Field data-invalid={isInvalid}>
+      {label && (
+        <CommonFieldLabel id={fieldId} label={label} required={required} />
+      )}
+      <Select
+        id={fieldId}
+        items={renderOptions}
+        value={value ?? undefined}
+        onValueChange={(v) => onValueChange?.(v ?? null)}
+        defaultValue={defaultValue ?? undefined}
+      >
+        <SelectTrigger aria-invalid={isInvalid} className={className}>
+          <SelectValue placeholder={placeholderOption} />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {selectLabel && <SelectLabel>{selectLabel}</SelectLabel>}
+            {renderOptions.map((option) => (
+              <SelectItem key={option.value} value={option.value}>
+                {option.label}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      {isInvalid && <FieldError>{error}</FieldError>}
+    </Field>
+  );
+};
+
+export default CommonSelect;
---
+++ src/components/shared/select/common-select.types.ts
@@ -0,0 +1,12 @@
+export interface CommonSelectProps {
+  id?: string;
+  options: { label: string; value: string | null }[];
+  value?: string | null;
+  onValueChange?: (value: string | null) => void;
+  defaultValue?: string | null;
+  selectLabel?: string;
+  label?: string;
+  className?: string;
+  error?: string;
+  required?: boolean;
+}
