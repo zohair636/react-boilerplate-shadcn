06dbbf5a1a42f9c0411f8c65b4e40826eff4a17e
zohair636 <hafiz.zohair2@gmail.com>
2026-05-28 23:44:17 +0500

    feat: add reusable dropdown component

---
+++ src/components/shared/dropdown/common-dropdown-types.ts
@@ -0,0 +1,23 @@
+import type { ReactNode } from "react";
+
+interface DropDownListItem {
+  label: string;
+  value: string;
+  onClick?: () => void;
+  disabled?: boolean | undefined;
+  shortcut?: string | undefined;
+  icon?: ReactNode | undefined;
+  subOptions?: DropDownListItem[];
+}
+
+interface DropDownOptionLabel {
+  label?: string | undefined;
+  items?: DropDownListItem[];
+}
+
+export interface CommonDropdownProps {
+  trigger: ReactNode;
+  options: DropDownOptionLabel[];
+  className?: string;
+  itemClassName?: string;
+}
---
+++ src/components/shared/dropdown/common-dropdown.tsx
@@ -0,0 +1,102 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import type { CommonDropdownProps } from "./common-dropdown-types";
+import { Fragment } from "react";
+import { RenderIcon } from "@/utils/icon-utils";
+import { cn } from "@/lib/utils";
+
+const CommonDropdown = ({
+  trigger,
+  options,
+  className,
+  itemClassName,
+}: CommonDropdownProps) => {
+  if (!options?.length) return null;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>{trigger}</DropdownMenuTrigger>
+      <DropdownMenuContent className={cn("w-auto max-w-56", className)}>
+        {options.map((option, optionIndex) => {
+          const items = option.items || [];
+          return (
+            <DropdownMenuGroup key={`${option.label}-${optionIndex}`}>
+              {option.label && (
+                <DropdownMenuLabel>{option.label}</DropdownMenuLabel>
+              )}
+              {items.map((item, itemIndex) => {
+                const {
+                  label,
+                  value,
+                  disabled,
+                  icon,
+                  onClick,
+                  shortcut,
+                  subOptions,
+                } = item;
+                const itemKey = `${option.label ?? "group"}-${value}-${itemIndex}`;
+                return (
+                  <Fragment key={itemKey}>
+                    <DropdownMenuItem onClick={onClick} disabled={disabled}>
+                      <RenderIcon src={icon} />
+                      <span
+                        className={cn("truncate min-w-0 flex-1", itemClassName)}
+                      >
+                        {label}
+                      </span>
+                      <DropdownMenuShortcut>{shortcut}</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    {!!subOptions?.length && (
+                      <>
+                        <DropdownMenuSeparator />
+                        {subOptions.map((subOption, subIndex) => {
+                          const {
+                            label,
+                            value,
+                            onClick,
+                            disabled,
+                            icon,
+                            shortcut,
+                          } = subOption;
+                          return (
+                            <DropdownMenuItem
+                              key={`${itemKey}-${value}-${subIndex}`}
+                              onClick={onClick}
+                              disabled={disabled}
+                            >
+                              <RenderIcon src={icon} />
+                              <span
+                                className={cn(
+                                  "truncate min-w-0 flex-1",
+                                  itemClassName,
+                                )}
+                              >
+                                {label}
+                              </span>
+                              <DropdownMenuShortcut>
+                                {shortcut}
+                              </DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                          );
+                        })}
+                      </>
+                    )}
+                  </Fragment>
+                );
+              })}
+            </DropdownMenuGroup>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default CommonDropdown;
