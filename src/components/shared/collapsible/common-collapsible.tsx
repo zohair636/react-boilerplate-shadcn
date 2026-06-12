6c810716bfaacedf5145654bb6de9f20419ea6a6
zohair636 <hafiz.zohair2@gmail.com>
2026-06-13 01:06:37 +0500

    feat(collapsible): basic & composed modes are added for more power

--- src/components/shared/collapsible/common-collapsible.tsx
+++ src/components/shared/collapsible/common-collapsible.tsx
@@ -5,20 +5,65 @@ import {
 } from "@/components/ui/collapsible";
 import type { CommonCollapsibleProps } from "./common-collapsible.types";
 
-const CommonCollapsible = ({
-  open,
-  onOpenChange,
-  trigger,
-  children,
-  className,
-  contentClassName,
-}: CommonCollapsibleProps) => {
+const CommonCollapsible = (props: CommonCollapsibleProps) => {
+  const {
+    open,
+    onOpenChange,
+    trigger,
+    defaultOpen = false,
+    disabled = false,
+    keepMounted = true,
+    children,
+    mode,
+    className,
+    contentClassName,
+    triggerClassName,
+  } = props;
+
+  const renderItems = () => {
+    if (mode === "basic") {
+      return (
+        <>
+          <CollapsibleTrigger className={triggerClassName}>
+            {trigger}
+          </CollapsibleTrigger>
+          <CollapsibleContent
+            keepMounted={keepMounted}
+            className={contentClassName}
+          >
+            {children}
+          </CollapsibleContent>
+        </>
+      );
+    }
+    if (mode === "composed") {
+      const { beforeTrigger, afterTrigger } = props;
+      return (
+        <>
+          {beforeTrigger}
+          <CollapsibleTrigger className={triggerClassName}>
+            {trigger}
+          </CollapsibleTrigger>
+          {afterTrigger}
+          <CollapsibleContent
+            keepMounted={keepMounted}
+            className={contentClassName}
+          >
+            {children}
+          </CollapsibleContent>
+        </>
+      );
+    }
+  };
   return (
-    <Collapsible open={open} onOpenChange={onOpenChange} className={className}>
-      <CollapsibleTrigger>{trigger}</CollapsibleTrigger>
-      <CollapsibleContent className={contentClassName}>
-        {children}
-      </CollapsibleContent>
+    <Collapsible
+      open={open}
+      onOpenChange={onOpenChange}
+      defaultOpen={defaultOpen}
+      disabled={disabled}
+      className={className}
+    >
+      {renderItems()}
     </Collapsible>
   );
 };
--- src/components/shared/collapsible/common-collapsible.types.ts
+++ src/components/shared/collapsible/common-collapsible.types.ts
@@ -1,10 +1,28 @@
 import type { ReactNode } from "react";
 
-export interface CommonCollapsibleProps {
+interface CollapsibleBasicModeConfig extends CollapsibleBasicProps {
+  mode: "basic";
+}
+
+interface CollapsibleComposedModeConfig extends CollapsibleBasicProps {
+  mode: "composed";
+  beforeTrigger?: ReactNode;
+  afterTrigger?: ReactNode;
+}
+
+type CollapsibleBasicProps = {
   open?: boolean;
   onOpenChange?: (open: boolean) => void;
   trigger: ReactNode;
   children: ReactNode;
+  defaultOpen?: boolean;
+  disabled?: boolean;
+  keepMounted?: boolean;
   className?: string;
   contentClassName?: string;
-}
+  triggerClassName?: string;
+};
+
+export type CommonCollapsibleProps =
+  | CollapsibleBasicModeConfig
+  | CollapsibleComposedModeConfig;
